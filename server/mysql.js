a7e78b757a8c98e07df80b73dda1e1cd2c2a319c
hwb0218 <52212226+hwb0218@users.noreply.github.com>
2021-01-23 20:02:27 +0900

    users 라우트 작업

--- server/mysql.js
+++ server/mysql.js
@@ -1,9 +1,11 @@
+const { password } = require('./secret/dev')
+
 var mysql = require('mysql2');
 // 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다.
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'dz731106hjh!',
+    password : password,
     database : 'yanolja'
 });
 
--- server/server.js
+++ server/server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
-const User = require('./lib/User');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const { auth } = require('./middleware/auth');
 const filter = require('../client/lib/filter');
 const date = require('../client/lib/date');
-const reservation = require('./lib/reservation');
 const { database } = require('./lib/database');
 const session = require('express-session');
 const sessionOptions = require('./lib/express-session-options');
@@ -21,6 +16,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session(sessionOptions));
+app.use('/api/users', require('./routes/user'));
+
 
 const isLoggedIn = (req, res, viewFile, render) => {
     const url = req.path;
@@ -186,32 +183,6 @@ app.get('/register', (req, res) => {
     });
 });
 
-app.post('/api/users/register', (req, res) => {
-    const request = req.body;
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        if (err) {
-            return res.send(err);
-        }
-        bcrypt.hash(request.password, salt, (err, hash) => {
-            if (err) {
-                return res.send(err);
-            }
-            request.password = hash;
-            database.query(`INSERT INTO user (email, password, name) VALUES (?, ?, ?)`,
-                [request.email, request.password, request.name],
-                (err, users) => {
-                    if (err) {
-                        return res.status(400).json({
-                            success: false, err
-                        });
-                    }
-                    return res.status(200).redirect('/');
-                }
-            );
-        });
-    });
-});
-
 app.get('/login', (req, res) => {
     fs.readFile('./client/views/login.html', 'utf8', (err, data) => {
         if (err) {
@@ -221,49 +192,6 @@ app.get('/login', (req, res) => {
     });
 });
 
-
-app.post('/api/users/login', (req, res) => {
-    const request = req.body;
-    database.query(`SELECT * FROM user WHERE email = ?`, [request.email], (err, users) => {
-        if (users.length === 0) {
-            return res.json({ success: false, message: '제공 된 이메일에 해당하는 유저가 없습니다.' });
-        }
-        User.comparePassword(request.password, users,function (err, isMatch) {
-            if(!isMatch) {
-                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."});
-            }
-            User.generateToken(users, function (err, user) {
-                if (err) {
-                    return res.status(400).send(err);
-                }
-                req.session.auth = user.token;
-                const reDirUrl = req.session.returnTo;
-                req.session.save(function () {
-                    res.status(200).redirect(reDirUrl);
-                });
-            });
-        });
-    });
-});
-
-app.get('/api/users/auth', auth, (req, res) => {
-    res.status(200).json({
-        id : req.user.id,
-        email : req.user.email,
-        name: req.user.name
-     });
-});
-
-app.get('/api/users/logout', auth, (req, res) => {
-    database.query(`UPDATE user SET token = ? WHERE email = ?`, [null, req.user.email], (err, user) => {
-        if (err) {
-            return res.json({ success: false, err });
-        }
-        const reDirUrl = req.session.returnTo;
-        delete req.session.auth;
-        return res.status(200).redirect(reDirUrl);
-    });
-});
 
 app.get('/motel', (req, res) => {
     fs.readFile('./client/views/motel.html', 'utf8', (err, data) => {
@@ -324,22 +252,6 @@ app.get('/mypage', (req, res) => {
                     isLoggedIn(req, res, 'mypage', { allRooms });
                 });
             });
-        });
-    });
-});
-
-app.get('/api/users/reservation', (req, res) => {
-    const selectedDate = filter.getObjValues(req, 'sel_date').filter(x => x);
-    const checkIn = selectedDate[0];
-    const checkOut = selectedDate[1];
-    const id = req.query.id;
-    database.query(`SELECT user.id FROM user WHERE token = ?`, [req.session.auth], (err ,data) => {
-        const values = reservation.reservation(checkIn, checkOut, id, data[0].id);
-        const roomType = req.query.roomType;
-        const roomTypeUpperCase = roomType.toUpperCase();
-        database.query(`INSERT INTO ${roomType}_reservation (RESERVATION_DATE, ${roomTypeUpperCase}_ID, USER_ID) VALUES ?`, [values], (err, result) => {
-            const reDirUrl = req.session.returnTo;
-            res.status(200).redirect(reDirUrl);
         });
     });
 });
